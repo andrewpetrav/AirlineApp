5148eb35549b6dd27777ecc7c2d9de9ee0a64618
andrewpetrav <61365800+andrewpetrav@users.noreply.github.com>
2021-05-21 15:41:45 -0500

    Create Utility.java

---
+++ Utility.java
@@ -0,0 +1,8 @@
+
+public class Utility {
+
+	public static final String url = "your url";
+	public static final String user = "your username";
+	public static final String password = "your password";
+
+}
